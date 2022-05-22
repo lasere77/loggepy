b66a2c26ee0fe7b592e7a254b0fbf72ffa3350be
lasere77 <87900050+lasere77@users.noreply.github.com>
2022-05-22 20:18:10 +0200

    Delete getpasseword.py
    this file has been moved elsewhere

--- getpasseword.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-
-def get_passeword():
-    choose = input("merci de mettre le nom de votre mot de passe : ")
-    load_dotenv(dotenv_path="passeworld/passewords")
-    print(os.getenv(choose))
